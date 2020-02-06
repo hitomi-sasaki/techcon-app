ec5bad5227a03a65a349f65bbced35bfa262e265
haru067 <haru.pdf@gmail.com>
2020-02-06 16:50:26 +0900

    Add back key and share button

--- android/src/main/java/jp/gree/techcon/composables/common/AppBar.kt
+++ android/src/main/java/jp/gree/techcon/composables/common/AppBar.kt
@@ -2,19 +2,28 @@ package jp.gree.techcon.composables.common
 
 import androidx.compose.Composable
 import androidx.ui.core.Text
-import androidx.ui.foundation.ColoredRect
-import androidx.ui.graphics.Color
-import androidx.ui.layout.*
-import androidx.ui.material.Scaffold
 import androidx.ui.material.TopAppBar
-import androidx.ui.material.surface.Surface
-import androidx.ui.unit.dp
+import jp.gree.techcon.R
+import jp.gree.techcon.composables.VectorImageButton
 import jp.gree.techcon.composables.lightColors
 
 @Composable
-fun AppBar(title: String) {
-    TopAppBar(
-        title = { Text(title) },
-        color = lightColors.background
-    )
+fun AppBar(title: String, onBackClick: (() -> Unit)? = null, onShareClick: (() -> Unit)? = null) {
+    val backIcon: @Composable (() -> Unit)? =
+        onBackClick?.let { { VectorImageButton(R.drawable.ic_back, it) } }
+    if (onShareClick != null) {
+        TopAppBar(
+            title = { Text(title) },
+            color = lightColors.background,
+            navigationIcon = backIcon,
+            action = { VectorImageButton(R.drawable.ic_share, onShareClick) },
+            actionData = listOf("hoge") // TODO: ?
+        )
+    } else {
+        TopAppBar(
+            title = { Text(title) },
+            color = lightColors.background,
+            navigationIcon = backIcon
+        )
+    }
 }
