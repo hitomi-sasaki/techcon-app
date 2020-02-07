06717fe0f7a3196262b2e692d2b1cc8adb519de9
keigo.masuzumi <keigo.masuzumi@gree.net>
2020-02-07 23:13:30 +0900

    Add JWT Based Authentication

--- server/src/jp/gree/techcon/server/Application.kt
+++ server/src/jp/gree/techcon/server/Application.kt
@@ -3,6 +3,9 @@ package jp.gree.techcon.server
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.authenticate
+import io.ktor.auth.authentication
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
@@ -13,18 +16,24 @@ import io.ktor.locations.Location
 import io.ktor.locations.Locations
 import io.ktor.locations.get
 import io.ktor.response.respond
-import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
+import io.ktor.routing.routing
 import io.ktor.serialization.serialization
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import jp.gree.techcon.common.model.*
+import io.ktor.util.KtorExperimentalAPI
+import jp.gree.techcon.common.model.Article
+import jp.gree.techcon.common.model.ArticleList
 import jp.gree.techcon.common.model.Session
+import jp.gree.techcon.common.model.SessionList
 import jp.gree.techcon.server.service.DatabaseFactory
 import jp.gree.techcon.server.service.SessionService
-import org.jetbrains.exposed.sql.*
+import jp.gree.techcon.server.service.firebaseUid
+import jp.gree.techcon.server.service.techcon
 
+
+@KtorExperimentalAPI
 @KtorExperimentalLocationsAPI
 fun Application.module() {
     DatabaseFactory.init(environment.config)
@@ -36,39 +45,47 @@ fun Application.module() {
     install(ContentNegotiation) {
         serialization()
     }
-    install(Routing) {
-        get("/") {
-            call.respond("hello")
+    install(Authentication) {
+        techcon {
+            gcpProject = environment.config.property("ktor.techcon.gcpProject").getString()
         }
-        @Location("/session/{id}")
-        data class SessionLocation(val id: Long)
-        get<SessionLocation> { param ->
-            val id = param.id
-            call.respond(Session.getSample())
-        }
-        get("/sessions") {
-            call.respond(
-                HttpStatusCode.OK,
-                SessionService().getAllSessions()
-            )
-        }
-        get("/bookmarks") {
-            call.respond(
-                HttpStatusCode.OK,
-                SessionList(Session.getDummyList().take(4))
-            )
-        }
-        @Location("article/{id}")
-        data class ArticleLocation(val id: Long)
-        get<ArticleLocation> { param ->
-            val id = param.id
-            call.respond(Article.getSample(id))
-        }
-        get("/articles") {
-            call.respond(ArticleList(Article.getSamples()))
-        }
-        post("/bookmark") {
-            call.respond(Session.getSample())
+    }
+    routing {
+        authenticate(optional = true) {
+            get("/") {
+                call.respond("hello")
+            }
+            @Location("/session/{id}")
+            data class SessionLocation(val id: Long)
+            get<SessionLocation> { param ->
+                val id = param.id
+                call.respond(Session.getSample())
+            }
+            get("/sessions") {
+                // val firebaseUid: String? = call.authentication.firebaseUid()
+                call.respond(
+                    HttpStatusCode.OK,
+                    SessionService().getAllSessions()
+                )
+            }
+            get("/bookmarks") {
+                call.respond(
+                    HttpStatusCode.OK,
+                    SessionList(Session.getDummyList().take(4))
+                )
+            }
+            @Location("article/{id}")
+            data class ArticleLocation(val id: Long)
+            get<ArticleLocation> { param ->
+                val id = param.id
+                call.respond(Article.getSample(id))
+            }
+            get("/articles") {
+                call.respond(ArticleList(Article.getSamples()))
+            }
+            post("/bookmark") {
+                call.respond(Session.getSample())
+            }
         }
     }
 }
