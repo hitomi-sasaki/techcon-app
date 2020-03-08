3ca3b82b6695b10fe7cca29bf274f19107006f4f
haru067 <haru.pdf@gmail.com>
2020-03-09 01:05:26 +0900

    Add map to lounge screen

--- android/src/main/java/jp/gree/techcon/screens/lounge/LoungeScreen.kt
+++ android/src/main/java/jp/gree/techcon/screens/lounge/LoungeScreen.kt
@@ -2,13 +2,14 @@ package jp.gree.techcon.screens.lounge
 
 import androidx.compose.Composable
 import androidx.ui.core.Alignment
+import androidx.ui.foundation.DrawImage
 import androidx.ui.foundation.VerticalScroller
-import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
 import androidx.ui.layout.Container
 import androidx.ui.layout.LayoutGravity
-import androidx.ui.material.surface.Surface
+import androidx.ui.res.imageResource
 import androidx.ui.unit.dp
+import jp.gree.techcon.R
 import jp.gree.techcon.common.model.Booth
 import jp.gree.techcon.composables.*
 
@@ -17,15 +18,17 @@ fun Lounge(booths: List<Booth>) {
     VerticalScroller {
         ScreenColumn {
             Column {
+                // TODO: Use text resource?
                 PrimaryText("ブースやフリーコーヒー、電源などを準備しています。休憩や、交流の場所としてご利用ください。", appTypography.body1)
                 VerticalSpace(24.dp)
-                Surface(color = Color(207, 207, 207)) {
-                    Container(
+                Container(
                         modifier = LayoutGravity.Center,
                         height = 280.dp,
                         width = 300.dp,
                         alignment = Alignment.Center
-                    ) {} // TODO: load image
+                ) {
+                    // TODO: Replace tmp image
+                    DrawImage(image = imageResource(id = R.drawable.tmp_map))
                 }
                 booths.forEach { Booth(it) }
             }
